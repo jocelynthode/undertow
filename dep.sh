254d8bc8157675337bae952b09da6ae215ca9f45
Simon Brulhart <simon@brulhart.me>
2016-04-25 17:24:34 +0200

    Give tomcat path in argument to dep.sh

--- dep.sh
+++ dep.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-mvn install:install-file -Dfile=./tomcat-juli.jar -DgroupId=org.apache.tomcat -DartifactId=tomcat-juli -Dversion=1.0 -Dpackaging=jar;
-mvn install:install-file -Dfile=./tomcat-util.jar -DgroupId=org.apache.tomcat -DartifactId=tomcat-util -Dversion=1.0 -Dpackaging=jar;
-mvn install:install-file -Dfile=./tomcat-jni.jar -DgroupId=org.apache.tomcat -DartifactId=tomcat-jni -Dversion=1.0 -Dpackaging=jar;
+# Path to tomcat repo (built)
+TOMCAT="$1"
+
+mvn install:install-file "-Dfile=$TOMCAT/output/build/bin/tomcat-juli.jar" -DgroupId=org.apache.tomcat -DartifactId=tomcat-juli -Dversion=1.0 -Dpackaging=jar;
+mvn install:install-file "-Dfile=$TOMCAT/output/build/lib/tomcat-util.jar" -DgroupId=org.apache.tomcat -DartifactId=tomcat-util -Dversion=1.0 -Dpackaging=jar;
+mvn install:install-file "-Dfile=$TOMCAT/output/build/lib/tomcat-jni.jar" -DgroupId=org.apache.tomcat -DartifactId=tomcat-jni -Dversion=1.0 -Dpackaging=jar;
